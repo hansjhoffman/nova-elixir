3905e68b7e0af98831d5a7d51b6e86946b08552a
Hans Hoffman <hoffman.hans.j@gmail.com>
2021-08-04 22:23:29 -0400

    add language server as submodule

--- shell.nix
+++ shell.nix
@@ -3,7 +3,28 @@
 with pkgs;
 
 let
-  basePackages = [ git nixfmt nodejs yarn ];
+  inherit (lib) optionals;
 
-  inputs = basePackages;
-in mkShell { buildInputs = inputs; }
+  basePackages = [ beam.packages.erlangR24.elixir_1_12 elixir_ls git nixfmt nodejs yarn ];
+
+  inputs = basePackages ++ optionals stdenv.isLinux inotify-tools
+    ++ optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+      # file_system used by phoenix_live_reload
+      CoreFoundation
+      CoreServices
+    ]);
+
+  hooks = ''
+    mkdir -p .nix-mix
+    mkdir -p .nix-hex
+    export MIX_HOME=$PWD/.nix-mix
+    export HEX_HOME=$PWD/.nix-hex
+    export PATH=$MIX_HOME/bin:$PATH
+    export PATH=$HEX_HOME/bin:$PATH
+    export LANG=en_US.UTF-8
+    export ERL_AFLAGS="-kernel shell_history enabled"
+  '';
+in mkShell {
+  buildInputs = inputs;
+  shellHooks = hooks;
+}
